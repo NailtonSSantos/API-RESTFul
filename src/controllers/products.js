c7cf926a83230486ca5b9b5afae23da44631b609
nailtonssantos <nltsilva8@gmail.com>
2022-03-31 20:39:47 -0400

    adicionando método GET

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,8 +1,11 @@
 const productsModel = require('../models/products')
 
 async function get(req, res){
-    const products = await productsModel.find()
-    
+    const { id } = req.params
+
+    const obj = id ? { _id: id } : null
+
+    const products = await productsModel.find(obj)
     
     res.send(products)
 }
